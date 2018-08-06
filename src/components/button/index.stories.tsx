280e0d9a699f53df38423d310a9ed41cea324bf9
Shuta Kumano <shoota.dev@gmail.com>
2018-08-06 16:31:11 +0900

    add example src

---
+++ src/components/button/index.stories.tsx
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+
+import BasicButton from '.'
+
+storiesOf('button/BasicButton', module).add('default', () => <BasicButton />)
