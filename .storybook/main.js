390d287eb780e104610bf785c7ed0cb6ca0ab515
Shoota Kumano <shoota.dev@gmail.com>
2021-12-03 23:59:07 +0900

    add less loading

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,24 +2,18 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
-  // webpackFinal: async (config) => ({
-  //   ...config,
-  //   module: {
-  //     ...config.module,
-  //     rules: [
-  //       {
-  //         test: /\.less$/,
-  //         use: [
-  //           'style-loader',
-  //           'css-loader',
-  //           {
-  //             loader: 'less-loader',
-  //             options: { javascriptEnabled: true },
-  //           },
-  //         ],
-  //       },
-  //       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-  //     ],
-  //   },
-  // }),
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.less$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'less-loader',
+          options: { javascriptEnabled: true },
+        },
+      ],
+    })
+    return config
+  },
 }
