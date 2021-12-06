32c0908acdc9a7df3fdb22eb09ea5f27b117728d
Shoota Kumano <shoota.dev@gmail.com>
2021-12-06 15:17:34 +0900

    improve data fetch

--- src/components/Todos/index.tsx
+++ src/components/Todos/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { List, Typography, Space } from 'antd'
+import { List, Typography, Space, Card, Button } from 'antd'
 import { CheckCircleTwoTone } from '@ant-design/icons'
 
 type Todo = {
@@ -11,27 +11,30 @@ type Todo = {
 type Props = {
   isLoading?: boolean
   todos?: Todo[]
+  getContent?: () => void
 }
 
-export const Todos: React.VFC<Props> = ({ todos, isLoading }) => {
+export const Todos: React.VFC<Props> = ({ todos, isLoading, getContent }) => {
   return (
-    <List
-      size="large"
-      loading={isLoading}
-      dataSource={todos}
-      renderItem={({ id, contents, finished }) => (
-        <List.Item key={id}>
-          <Typography.Title>
-            <Space>
-              <CheckCircleTwoTone
-                twoToneColor={finished ? '#52c41a' : '#e3e3e3'}
-              />
-              {contents}
-            </Space>
-          </Typography.Title>
-          {/* <Typography.Title level={5}>{contents}</Typography.Title> */}
-        </List.Item>
-      )}
-    />
+    <Card title="TODO List">
+      <Button onClick={getContent}>データを取る</Button>
+      <List
+        size="large"
+        loading={isLoading}
+        dataSource={todos}
+        renderItem={({ id, contents, finished }) => (
+          <List.Item key={id}>
+            <Typography.Title>
+              <Space>
+                <CheckCircleTwoTone
+                  twoToneColor={finished ? '#52c41a' : '#e3e3e3'}
+                />
+                {contents}
+              </Space>
+            </Typography.Title>
+          </List.Item>
+        )}
+      />
+    </Card>
   )
 }
--- src/container/Todos/index.tsx
+++ src/container/Todos/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { WatchQueryFetchPolicy } from '@apollo/client'
 
 import { Todos } from 'components/Todos'
-import { useTodosQuery } from 'graphql/api'
+import { useTodosLazyQuery } from 'graphql/api'
 
 type Props = {
   fetchPolicy: WatchQueryFetchPolicy
 }
 
 export const TodosContainer: React.FC<Props> = ({ fetchPolicy }) => {
-  const { data, loading } = useTodosQuery({ fetchPolicy })
-  return <Todos todos={data?.todos} isLoading={loading} />
+  const [getTodo, { data, loading }] = useTodosLazyQuery({ fetchPolicy })
+  const getContent = useCallback(() => getTodo(), [getTodo])
+  return (
+    <Todos todos={data?.todos} isLoading={loading} getContent={getContent} />
+  )
 }
